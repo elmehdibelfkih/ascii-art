4ee9e97d5442c4ed19884cf9b2fe97601652b7c0
elmehdibelfkih <elmehdibelfkih@gmail.com>
2024-12-31 19:15:21 +0100

    working version

--- internal/fileTools.go
+++ internal/fileTools.go
@@ -10,13 +10,18 @@ import (
 func ParsFile(font string) map[rune][]string {
 	ret :=  make(map[rune][]string)
 
+	
 	data, err := os.ReadFile(font)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	data = []byte(strings.Trim(string(data), "\n"))
 	tmp := strings.Split(string(data), "\n\n")
+	// println("han>", tmp[0])
 	for i, elm := range config.Content {
+		// if i > len(tmp) - 1 {
+		// 	break
+		// }
 		ret[elm] = strings.Split(tmp[i], "\n")
 	}
 	return ret
--- internal/printAscii.go
+++ internal/printAscii.go
@@ -1,15 +1,23 @@
 package internal
 
 import (
-	"fmt"
 	"ascii-art/config"
+	"fmt"
+	"strings"
 )
 
 func PrintStringAscii(str string) {
-	for i := 0; i < config.CharacterHeight; i++{
-		for _, c := range str {
-			fmt.Print(config.Standard[c][i])
+	tmp := strings.Split(str, `\n`)
+	for _, line := range tmp {
+		if line != "" {
+			for i := 0; i < config.CharacterHeight; i++{
+				for _, c := range line {
+					fmt.Print(config.Standard[c][i])
+				}
+				fmt.Println()
+			}
+		} else {
+			fmt.Println()
 		}
-		fmt.Println()
 	}
 }
