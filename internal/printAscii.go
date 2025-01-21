d0102acdb54c62954a8574d82cbde3607b29754d
elmehdibelfkih <elmehdibelfkih@gmail.com>
2025-01-21 15:42:51 +0100

    hashing the banner

--- internal/printAscii.go
+++ internal/printAscii.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"crypto/md5"
 	"fmt"
 	"log"
 	"os"
@@ -10,9 +11,19 @@ import (
 var Standard = make(map[rune][]string, 95)
 
 const CharacterHeight = 8
+const originalHash = "ac85e83127e49ec42487f272d9b9db8b"
 
 func ParsFile(fontFileName string) {
 	data, err := os.ReadFile(fontFileName)
+	hashObj := md5.New()
+	hashObj.Write(data)
+	hash := fmt.Sprintf("%x", hashObj.Sum(nil))
+	if string(hash) != originalHash {
+		fmt.Fprintf(os.Stderr, "Error: Hash does not match!\n")
+		fmt.Fprintf(os.Stderr, "Computed Hash: %s\n", hash)
+		fmt.Fprintf(os.Stderr, "Expected Hash: %s\n", originalHash)
+		os.Exit(0)
+	}
 	if err != nil {
 		log.Fatalln(err)
 	}
